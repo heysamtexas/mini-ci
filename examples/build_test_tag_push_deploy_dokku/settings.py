f76094437e043a0804272d0ea353e82f322473de
Mark Anderson <mark@m3b.net>
2017-03-01 00:41:09 +0100

    added settings.py to example. it was previously ignored

---
+++ examples/build_test_tag_push_deploy_dokku/settings.py
@@ -0,0 +1,11 @@
+settings = {
+    "project_1": {
+        "GIT_REPO": "git@github.com:username/project_1.git",
+        "DOKKU_APP_NAME": "project_1",
+        "DOKKU_HOST": "mydokkuhost.com",
+        "DOKKU_DEPLOY_USER": "deployuser",
+        "DOCKER_USER": "myuser",
+        "DOCKER_PASS": "mypass",
+        "IMAGE_NAME": "username/project_1"
+    },
+}
